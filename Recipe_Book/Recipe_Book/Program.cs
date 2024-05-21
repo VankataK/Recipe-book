d27465cf360a248bd55c961de1c0144a848cab07
VankataK <ivan.kivshanov@gmail.com>
2024-05-21 12:07:04 +0300

    Project creation and folders

---
+++ Recipe_Book/Recipe_Book/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Recipe_Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
