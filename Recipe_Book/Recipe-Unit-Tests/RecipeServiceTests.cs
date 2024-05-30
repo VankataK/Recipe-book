80077a2142101609715d542f5f3e4a05f30e348c
Kaloyan Ganev <115919610+koko462@users.noreply.github.com>
2024-05-30 12:00:26 +0300

    nothing

---
+++ Recipe_Book/Recipe-Unit-Tests/RecipeServiceTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using Microsoft.EntityFrameworkCore;
+using Recipe_Book.Data;
+using Recipe_Book.Models;
+using Recipe_Book.Services;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+
+namespace Recipe_Book.Tests.Services
+{
+    [TestFixture]
+    public class RecipeServiceTests
+    {
+        
+    }
+}
+
